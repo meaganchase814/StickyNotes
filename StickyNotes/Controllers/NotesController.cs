e96a9b735148771d2a6e29ae73ee22bd38cad87f
Meagan Chase <meaganchase814@yahoo.com>
2018-09-26 16:50:47 -0400

    try 2

---
+++ StickyNotes/Controllers/NotesController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StickyNotes.Models;
+
+namespace StickyNotes.Controllers
+{
+    public class NotesController : Controller
+    {
+        private NotesContext db = new NotesContext();
+
+       public ActionResult Analyze(string Title, string Text, DateTime Now, int Category)
+        {
+            NotesContext CatList = new NotesContext();
+            Notes notenotes = new Notes();
+            notenotes.Title = Title;
+            notenotes.Text = Text;
+            notenotes.Now = Now;
+            notenotes.Category = new List<Group>();
+            notenotes.Category.Add(CatList.Category.Find(Category));
+            CatList.Notes.Add(notenotes);
+
+            CatList.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // GET: Notes
+        public ActionResult Index()
+        {
+            return View(db.Notes.ToList());
+        }
+
+        // GET: Notes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Notes notes = db.Notes.Find(id);
+            if (notes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(notes);
+        }
+
+        // GET: Notes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Notes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,Text,Now")] Notes notes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Notes.Add(notes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(notes);
+        }
+
+        // GET: Notes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Notes notes = db.Notes.Find(id);
+            if (notes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(notes);
+        }
+
+        // POST: Notes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,Text,Now")] Notes notes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(notes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(notes);
+        }
+
+        // GET: Notes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Notes notes = db.Notes.Find(id);
+            if (notes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(notes);
+        }
+
+        // POST: Notes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Notes notes = db.Notes.Find(id);
+            db.Notes.Remove(notes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ StickyNotes/Models/Notes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace StickyNotes.Models
+{
+    public class Notes
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public virtual List<Group> Category { get; set; }
+        public DateTime Now { get; set; }
+    }
+    public class Group
+    {
+        public int ID { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class NotesContext : DbContext
+    {
+        public DbSet<Notes> Notes { get; set; }
+        public DbSet<Group> Category { get; set; }
+    }
+    
+}
